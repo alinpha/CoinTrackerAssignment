3930d5f53655eff9f6eefe26412ab9a7944413d4
Aline Vetrov <alinpha@gmail.com>
2021-04-08 14:36:59 -0300

    worker completed

--- CoinTrackerStarter/app/src/main/java/ca/burchill/cointracker/work/CoinTrackerApplication.kt
+++ CoinTrackerStarter/app/src/main/java/ca/burchill/cointracker/work/CoinTrackerApplication.kt
@@ -1,7 +1,38 @@
 package ca.burchill.cointracker.work
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class CoinTrackerApplication : Application() {
-    // TODO - implement delayedInit() that ensures workmanager is refreshing the database regularly
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    private fun setupRecurringWork() {
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(15, TimeUnit.MINUTES)
+            .build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            Timber.plant(Timber.DebugTree())
+            setupRecurringWork()
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
 }
